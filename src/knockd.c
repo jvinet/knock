5aa99237b977eeca1dcfc296baf6e729d04cb40e
airwoflgh <paul.rogers@flumps.org>
2015-11-27 13:35:20 -0500

    Increment version number post Issue #16 fixes
    Version now 0.7.2

--- src/knockd.c
+++ src/knockd.c
@@ -49,7 +49,7 @@
 #include <errno.h>
 #include "list.h"
 
-static char version[] = "0.7.1";
+static char version[] = "0.7.2";
 
 #define SEQ_TIMEOUT 25 /* default knock timeout in seconds */
 #define CMD_TIMEOUT 10 /* default timeout in seconds between start and stop commands */

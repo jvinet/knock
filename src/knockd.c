1f77a3fd54e949b5c747dc80a3b2d77ba781c337
TDFKAOlli <TDFKAOlli@ish.de>
2020-06-28 22:58:32 +0200

    Fix incorrect TCP ACK/URG flag NOT_SET testing logic in flags_match() fetched from jvinet/knock#68 created by catbref

--- src/knockd.c
+++ src/knockd.c
@@ -1487,7 +1487,7 @@ int flags_match(opendoor_t* door, int ipTPoto, struct tcphdr* tcp)
 				dprint("packet is not ACK, ignoring...\n");
 				return 0;
 			}
-			if(door->flag_ack == NOT_SET && !(tcp->th_flags & TH_ACK)) {
+			if(door->flag_ack == NOT_SET && (tcp->th_flags & TH_ACK)) {
 				dprint("packet is not !ACK, ignoring...\n");
 				return 0;
 			}
@@ -1497,7 +1497,7 @@ int flags_match(opendoor_t* door, int ipTPoto, struct tcphdr* tcp)
 				dprint("packet is not URG, ignoring...\n");
 				return 0;
 			}
-			if(door->flag_urg == NOT_SET && !(tcp->th_flags & TH_URG)) {
+			if(door->flag_urg == NOT_SET && (tcp->th_flags & TH_URG)) {
 				dprint("packet is not !URG, ignoring...\n");
 				return 0;
 			}
